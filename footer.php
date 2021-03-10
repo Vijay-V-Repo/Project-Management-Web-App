dc4e929ce7ecdd01388eee2fb212a0fbd7038a4a
Vijay <74922968+vijaycs20@users.noreply.github.com>
2021-03-10 23:13:12 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,4 @@
+
+                <footer class="footer">
+                    © 2021 Project Collab <span class="d-none d-md-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Code Crew</span>
+                </footer>
