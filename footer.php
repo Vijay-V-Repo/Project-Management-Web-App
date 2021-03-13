c1ef5bf8ddbd1bbe3222af8981567e0d300fb242
Vijay <74922968+vijaycs20@users.noreply.github.com>
2021-03-13 23:28:24 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,4 +0,0 @@
-
-                <footer class="footer">
-                    © 2021 Project Collab <span class="d-none d-md-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Code Crew</span>
-                </footer>
